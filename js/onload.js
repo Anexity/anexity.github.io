5195fc69a98638744e3f80750bfc55d94da74646
Nex <Anexity@protonmail.com>
2025-08-20 13:13:50 +0200

    push offline fixes
    do mobile version
    fix portfolio thingy
    do a bunch of reworking

--- js/onload.js
+++ js/onload.js
@@ -7,3 +7,11 @@ function closeSocials() {
 	document.getElementById("cli1").classList.remove("clickedinfo2");
 	document.getElementById("cli1").classList.add("clickedinfo");
 }
+
+function openPort() {
+	document.getElementById("port1").classList.remove("hide")
+}
+
+function closePort() {
+	document.getElementById("port1").classList.add("hide")
+}
